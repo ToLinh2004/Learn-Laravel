3458e2255092e7f608e2b82f9183bc68ea9f9481
Linh <nguyenthilinhqni2020@gmail.com>
2024-03-06 21:48:35 +0700

    response redirect

--- resources/views/clients/demo-test.blade.php
+++ resources/views/clients/demo-test.blade.php
@@ -1,6 +1,12 @@
 <h2>Demo view unicode</h2>
-<h2>
+{{-- <h2>
     @foreach ($data as $key=>$item)
         {{ $key }}
     @endforeach
-</h2>
+</h2> --}}
+
+<form action="" method='post'>
+@csrf
+    <input type="text" name='username'>
+    <button type='submit'>Submit</button>
+</form>
